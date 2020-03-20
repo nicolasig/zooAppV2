06541ebfe3799ade86253faab17c31ba6a48703d
Nicolas <nicolasig@hotmail.com>
2020-03-20 12:06:27 +0000

    hello world

---
+++ HelloWorld/Main.java
@@ -0,0 +1,11 @@
+package Learning;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main (String[] args) throws IOException {
+		System.out.println("Hola Mundo.");
+	}
+	
+}
